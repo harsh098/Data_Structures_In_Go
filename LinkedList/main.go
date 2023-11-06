00eca63043a8ad6584e3fb3e0630926cd5b9a720
Harsh Mishra <hmisraji07@gmail.com>
2023-11-06 06:08:14 +0530

    Added Code for Linked List in Golang
    Signed-off-by: Harsh Mishra <hmisraji07@gmail.com>

---
+++ LinkedList/main.go
@@ -0,0 +1,100 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	data int
+	next *Node
+}
+
+type LinkedList struct {
+	head   *Node
+	length int
+}
+
+func (n *Node) Value() int {
+	return n.data
+}
+
+func (n *Node) Next() (next *Node) {
+	next = n.next
+	return next
+}
+
+func NewList() *LinkedList {
+	list := &LinkedList{head: nil, length: 0}
+	return list
+}
+
+func NewNode(value int) *Node {
+	return &Node{data: value, next: nil}
+}
+
+func (l *LinkedList) AppendNode(n *Node) {
+	if n == nil {
+		return
+	}
+
+	if l.head == nil {
+		l.head = n
+		return
+	}
+
+	curr := l.head
+
+	for curr.next != nil {
+		curr = curr.Next()
+	}
+
+	curr.next = n
+}
+
+func (l *LinkedList) Add(value int) {
+	node := NewNode(value)
+	l.AppendNode(node)
+}
+
+func (l *LinkedList) Remove(value int) {
+	if l.head == nil {
+		return
+	}
+
+	if l.head.Value() == value {
+		l.head = l.head.Next()
+		return
+	}
+
+	curr := l.head
+
+	for curr.Next() != nil && curr.Next().Value() != value {
+		curr = curr.Next()
+	}
+
+	if curr.Next() != nil {
+		curr.next = curr.Next().Next()
+	}
+}
+
+func main() {
+	list := NewList()
+	list.Add(1)
+	list.Add(2)
+	list.Add(3)
+	list.Add(4)
+	printList(list)
+	list.Remove(3)
+	printList(list)
+	list.Remove(1)
+	printList(list)
+	fmt.Println(list.head)
+
+}
+
+func printList(l *LinkedList) {
+	curr := l.head
+	fmt.Println("")
+	for ; curr != nil; curr = curr.Next() {
+		fmt.Printf("%v", curr.Value())
+	}
+	fmt.Println("")
+}
