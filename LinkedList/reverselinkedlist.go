c7fed381941f771fba254e2cc685b18c81d2a3e9
Harsh Mishra <hmisraji07@gmail.com>
2023-11-18 08:38:25 +0530

    Added Reversal of Linked List Via Stack
    Signed-off-by: Harsh Mishra <hmisraji07@gmail.com>

---
+++ LinkedList/reverselinkedlist.go
@@ -0,0 +1,24 @@
+package main
+
+func (l* LinkedList) ReverseWithAStack(){
+	if l.length < 2 {
+		return
+	}
+	curr := l.head
+	stack := make([](*Node),0,l.length)
+	for curr!=nil {
+		stack = append(stack, curr)
+		curr = curr.Next()
+	}
+	j := len(stack)-1
+	l.head = stack[j]
+	curr = l.head
+	j--
+	for j>-1 {
+		curr.next = nil
+		curr.next = stack[j]
+		curr=curr.next
+		j--
+	}
+	curr.next = nil
+}	
